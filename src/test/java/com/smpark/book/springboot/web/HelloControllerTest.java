04823972ef9feadf096ea418eb3a74f12b3ba852
smpark1020 <smpark911020@gmail.com>
2021-04-09 17:22:08 +0900

    2.4 Hello Controller 코드를 롬복으로 전환하기 ~ 3.2 프로젝트에 Spring Data Jpa 적용하기

--- src/test/java/com/smpark/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/smpark/book/springboot/web/HelloControllerTest.java
@@ -1,7 +1,9 @@
 package com.smpark.book.springboot.web;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -25,6 +27,19 @@ public class HelloControllerTest {
 		mvc.perform(get("/hello"))
 			.andExpect(status().isOk())
 			.andExpect(content().string(hello));
+	}
+	
+	@Test
+	public void helloDto가_리턴된다() throws Exception {
+		String name = "hello";
+		int amount = 1000;
+		
+		mvc.perform(get("/hello/dto")
+				.param("name", name)
+				.param("amount", String.valueOf(amount)))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.name", is(name)))
+		.andExpect(jsonPath("$.amount", is(amount)));
 	}
 	
 }
