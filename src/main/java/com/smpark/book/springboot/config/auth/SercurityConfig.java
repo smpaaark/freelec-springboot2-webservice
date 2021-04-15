6ece0405389a7d22acd5c65806922319b99f93cc
smpark1020 <smpark911020@gmail.com>
2021-04-15 16:51:53 +0900

    5.2 구글 서비스 등록 ~ 5.4 어노테이션 기반으로 개선하기(ing)

---
+++ src/main/java/com/smpark/book/springboot/config/auth/SercurityConfig.java
@@ -0,0 +1,37 @@
+package com.smpark.book.springboot.config.auth;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.smpark.book.springboot.domain.user.Role;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SercurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final CustomOauth2UserService customOauth2UserService;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.headers().frameOptions().disable()
+			.and()
+				.authorizeRequests()
+				.antMatchers("/", "/css/**", "/images/**",
+						"/js/**", "/h2-console/**").permitAll()
+				.antMatchers("/api/v1/**").hasRole(Role.USER.name())
+				.anyRequest().authenticated()
+			.and()
+				.logout()
+					.logoutSuccessUrl("/")
+			.and()
+				.oauth2Login()
+					.userInfoEndpoint()
+						.userService(customOauth2UserService);
+	}
+	
+}
---
+++ src/main/java/com/smpark/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,22 @@
+package com.smpark.book.springboot.config.auth.dto;
+
+import java.io.Serializable;
+
+import com.smpark.book.springboot.domain.user.User;
+
+import lombok.Getter;
+
+@Getter
+public class SessionUser implements Serializable {
+	
+	private String name;
+	private String email;
+	private String picture;
+	
+	public SessionUser(User user) {
+		this.name = user.getName();
+		this.email = user.getEmail();
+		this.picture = user.getPicture();
+	}
+	
+}
