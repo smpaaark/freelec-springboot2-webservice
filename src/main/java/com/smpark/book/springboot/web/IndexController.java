f7594e096a600ae9b469230308d5af879257037d
smpark1020 <smpark911020@gmail.com>
2021-04-19 11:37:40 +0900

    5.4 어노테이션 기반으로 개선하기

--- src/main/java/com/smpark/book/springboot/web/IndexController.java
+++ src/main/java/com/smpark/book/springboot/web/IndexController.java
@@ -1,12 +1,11 @@
 package com.smpark.book.springboot.web;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.smpark.book.springboot.config.auth.LoginUser;
 import com.smpark.book.springboot.config.auth.dto.SessionUser;
 import com.smpark.book.springboot.service.posts.PostsService;
 import com.smpark.book.springboot.web.dto.PostsResponseDto;
@@ -18,13 +17,11 @@ import lombok.RequiredArgsConstructor;
 public class IndexController {
 	
 	private final PostsService postsService;
-	private final HttpSession httpSession;
 	
 	@GetMapping("/")
-	public String index(Model model) {
+	public String index(Model model, @LoginUser SessionUser user) {
 		model.addAttribute("posts", postsService.findAllDesc());
 		
-		SessionUser user = (SessionUser)httpSession.getAttribute("user");
 		if (user != null) {
 			model.addAttribute("userName", user.getName());
 		}
